faa955b61ea86781b63c5694e2ff0e723d154afc
cleiveliu <cleiveliu1@gmail.com>
2019-12-02 01:19:48 +0800

    auto commit

---
+++ 0371.两整数之和/0371-两整数之和.rs
@@ -0,0 +1,5 @@
+impl Solution {
+    pub fn get_sum(a: i32, b: i32) -> i32 {
+        if b == 0 { a } else { Solution::get_sum(a^b, (a&b)<<1) }
+    }
+}
---
+++ 0389.找不同/0389-找不同.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        arr1 = [0]*26 
+        arr2 = [0]*26
+        for c in s:
+            arr1[ord(c)-97] += 1 # ord('a') = 97
+        for c in t:
+            arr2[ord(c)-97] += 1
+        
+        for i in range(26):
+            if arr1[i] != arr2[i]:
+                return chr(i+97)
+        
+        """better version
+        n = 0
+        for c in s:
+            n ^= ord(c)
+        for c in t:
+            n ^= ord(c)
+        return chr(n)
+        """
+        
